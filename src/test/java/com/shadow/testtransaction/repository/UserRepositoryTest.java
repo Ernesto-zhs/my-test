9154f7ab5127883dbe70d42257036609bba0f00c
zhs <austin_zhs@163.com>
2023-03-23 09:37:00 +0800

    update:添加SQL打印插件

--- src/test/java/com/shadow/testtransaction/repository/UserRepositoryTest.java
+++ src/test/java/com/shadow/testtransaction/repository/UserRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.shadow.testtransaction.repository;
 
 import com.shadow.testtransaction.entity.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -14,12 +17,18 @@ class UserRepositoryTest {
 
     @Test
     void saveTest() {
-        for (int i = 0; i < 100; i++) {
-            User entity = new User();
-            entity.setAge(i);
-            entity.setName("my name is " + i);
-            userRepository.save(entity);
-        }
+        Assertions.assertDoesNotThrow(() -> {
+            for (int i = 0; i < 10; i++) {
+                List<User> userList = new ArrayList<>();
+                for (int j = 0; j < 10; j++) {
+                    User entity = new User();
+                    entity.setAge(i * j);
+                    entity.setName("my name is " + i * j);
+                    userList.add(entity);
+                }
+                userRepository.saveBatch(userList);
+            }
+        });
     }
 
 }
