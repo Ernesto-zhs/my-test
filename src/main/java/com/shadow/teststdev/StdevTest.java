cc0e117492c99f88c464ef7799a17bc24915b379
zhs <austin_zhs@163.com>
2023-04-18 15:49:39 +0800

    update:样本标准差

---
+++ src/main/java/com/shadow/teststdev/StdevTest.java
@@ -0,0 +1,23 @@
+package com.shadow.teststdev;
+
+import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
+
+import java.util.Random;
+
+/**
+ * 样本标准差
+ */
+public class StdevTest {
+
+    public static void main(String[] args) {
+        StandardDeviation standardDeviation = new StandardDeviation();
+        double[] values = new double[5];
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            values[i] =  random.nextInt(1000);
+        }
+        double evaluate = standardDeviation.evaluate(values);
+        System.out.println(evaluate);
+    }
+
+}
